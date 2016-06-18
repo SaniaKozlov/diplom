4694113f95b50c72a5cfc41a0cc363d94c6398c4
riki343 <vladislav.kosko@gmail.com>
2016-06-18 22:04:53 +0300

    fix image

--- src/DiplomBundle/Controller/UserController.php
+++ src/DiplomBundle/Controller/UserController.php
@@ -6,6 +6,7 @@ use DiplomBundle\Entity\User;
 use DiplomBundle\Exception\WrongDateException;
 use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Finder\Finder;
@@ -14,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
+ * @Security("has_role('ROLE_USER')")
  * @Route("/user")
  */
 class UserController extends Controller
